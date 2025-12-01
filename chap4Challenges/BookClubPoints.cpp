2ccaad47ffb2e96662acb651884eeb05ed315a9c
GzCodespace <103735@wws.k12.in.us>
2025-12-01 10:22:26 -0500

    message2

--- chap4Challenges/BookClubPoints.cpp
+++ chap4Challenges/BookClubPoints.cpp
@@ -1,1 +1,32 @@
-//commit comment
+// commit comment
+#include <iostream>
+using namespace std;
+int main()
+{
+    int books;
+    cout << "\nBooks Bought: ";
+    while (!(cin >> books))
+    {
+        cout << "Try again. Books Bought: ";
+        cin.clear();
+        cin.ignore(100, '\n');
+    }
+    switch (books)
+    {
+    case 0:
+        cout << "You got 0 points!";
+        break;
+    case 1:
+        cout << "You got 5 points!";
+        break;
+    case 2:
+        cout << "You got 15 points!";
+        break;
+    case 3:
+        cout << "You got 30 points!";
+        break;
+    default:
+        cout<<"You got 50 points!";
+        break;
+    }
+}
