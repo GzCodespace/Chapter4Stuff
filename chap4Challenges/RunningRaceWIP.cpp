30dc397fa252df9092d6ad62c650e9cf35e9903a
GzCodespace <103735@wws.k12.in.us>
2025-12-01 14:19:43 -0500

    message2

---
+++ chap4Challenges/RunningRaceWIP.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    string name1;
+    cout << "\nInput first racers name: ";
+    cin >> name1;
+    double time1;
+    cout << "Input "<< name1 <<"'s time : ";
+     while (!(cin >> time1))
+    {
+        cout << "Try again. Time: ";
+        cin.clear();
+        cin.ignore(100, '\n');
+    }
+    string name2;
+    cout << "\nInput second racers name: ";
+    cin >> name2;
+    double time2;
+    cout << "Input "<<name2<<"'s time : ";
+     while (!(cin >> time2))
+    {
+        cout << "Try again. Time: ";
+        cin.clear();
+        cin.ignore(200, '\n');
+    }
+    string name3;
+    cout << "\nInput third racers name: ";
+    cin >> name3;
+    double time3;
+    cout << "Input "<<name3<<"'s time : ";
+     while (!(cin >> time3))
+    {
+        cout << "Try again. Time: ";
+        cin.clear();
+        cin.ignore(300, '\n');
+    }
+    cout<<time1<<time2<<time3;
+    if (time1 < time2 && time1 < time3){
+        if (time2 < time3){
+            cout << "\n" << name1 << "was first.";
+            cout << "\n" << name2 << "was second.";
+            cout << "\n" << name3 << "was last.";
+        }if (time2 > time3){
+            cout << "\n" << name1 << "was first.";
+            cout << "\n" << name2 << "was second.";
+            cout << "\n" << name3 << "was last.";
+        }
+    }
+    
+    
+}
