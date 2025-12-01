af149f9fc1e0b240595caf85f8c5a077683060ce
GzCodespace <103735@wws.k12.in.us>
2025-12-01 10:40:15 -0500

    message2

--- chap4Challenges/SoftwareSales.cpp
+++ chap4Challenges/SoftwareSales.cpp
@@ -1,1 +1,36 @@
-//commit comment
+
+// commit comment
+#include <iostream>
+using namespace std;
+int main()
+{
+    int packages;
+    cout << "\nPackages: ";
+    while (!(cin >> packages))
+    {
+        cout << "Try again. packages: ";
+        cin.clear();
+        cin.ignore(100, '\n');
+    }
+    if (10 > packages)
+    {
+        cout << "Your total is: $" << packages*199;
+    }
+    else if (10 <= packages && packages < 20)
+    {
+        cout << "Your total is: $" << packages*199*0.8;
+    }
+    else if (20 <= packages && packages < 50)
+    {
+        cout << "Your total is: $" << packages*199*0.7;
+    }
+    else if (50 <= packages && packages < 100)
+    {
+        cout << "Your total is: $" << packages*199*0.6;
+    }
+    else if (packages >= 100)
+    {
+        cout << "Your total is: $" << packages*199*0.5;
+    }
+    
+}
